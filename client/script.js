39187e3605289587b3c9517b9a4033ddfad9d9bd
System Administrator <root@rice-mbp.local>
2019-08-06 17:03:12 -0500

    added some styling

--- client/script.js
+++ client/script.js
@@ -2,28 +2,68 @@
 const baseURL = 'http://localhost:5000/findPrice?album='
 const total = document.getElementById('total')
 const albumList = document.getElementById('collection')
+const recordImgs = document.getElementById('record-imgs')
+const addButton = document.getElementById('add-button')
+const albumName = document.getElementById('album-name')
+
+let numOfRecords = 0
+
+// bind 'Add' button to enter key
+document.onkeydown = function (e) {
+    e = e || window.event;
+    switch (e.which || e.keyCode) {
+        case 13 : addButton.click()
+    }
+}
+
+const updateTotal =(res)=> {
+    total.innerHTML = (parseFloat(total.innerHTML) + parseFloat(res.price.substring(1))).toFixed(2)
+}
+
+const setAlbumArt =(res)=> {
+    let imgNode = document.createElement('img')
+    imgNode.src = res.album_art
+    imgNode.style.zIndex = numOfRecords
+    imgNode.classList.add('album')
+    imgNode.style.position = 'relative'
+    imgNode.style.marginLeft = numOfRecords == 0 ? 0 : '-75px'
+    imgNode.addEventListener("mouseenter", function() {
+        this.style.zIndex = 30
+        this.width = 150
+        this.height = 150
+    })
+    let currentNum = numOfRecords
+    imgNode.addEventListener("mouseleave", function() {
+        this.style.zIndex = numOfRecords
+        this.width = 100
+        this.height = 100
+    })
+    numOfRecords += 1
+    imgNode.width = 100
+    imgNode.height = 100
+    return imgNode
+}
+
+const setNameAndPrice =(res)=> {
+    let listNode = document.createElement('li');
+    let textnode = document.createTextNode('   ' + res.album + ' : ' + res.price)
+    listNode.appendChild(textnode)
+    return listNode
+}
 
 const scrape =()=> {
-    let album = document.getElementById('albumName').value
-    console.log(baseURL + album.replace(/ /g,'+'))
+    // let album = document.getElementById('albumName').value.replace(/ /g,'+')
+    let album = albumName.value.replace(/ /g,'+')
+    addButton.disabled = true
     fetch(baseURL + album)
         .then(response => {return response.json()})
         .then(response => {
-            // set album name and price
-            total.innerHTML = (parseFloat(total.innerHTML) + parseFloat(response.price.substring(1))).toFixed(2)
-            let listNode = document.createElement('li');
-            let textnode = document.createTextNode('   ' + album + ' : ' + response.price)
-
-            // set album art
-            let imgNode = document.createElement('img')
-            imgNode.src = response.album_art
-            imgNode.width = 100
-            imgNode.height = 100
-            
-            // append name, price, and art to list
-            listNode.appendChild(imgNode)
-            listNode.appendChild(textnode)
-            albumList.appendChild(listNode)  
+            console.log(response)
+            updateTotal(response)
+            recordImgs.appendChild(setAlbumArt(response))
+            albumList.appendChild(setNameAndPrice(response))
+            addButton.disabled = false
+            albumName.value = ''
         })
-        .catch(err => console.error('Caught error: ', err))    
+        .catch(err => console.error('Caught error: ', err))
 }
--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ def get_album_price():
     full_url = disc_url_1 + disc_url_2 + album + disc_url_3
     result = scrape(full_url)
     #album_info = jsonify({'album': album})
-    album_info = jsonify({'album': album, 'price': result[0], 'album_art': result[1]})
+    album_info = jsonify({'album': result[2], 'price': result[0], 'album_art': result[1], 'artist': result[3]})
     album_info.headers.add('Access-Control-Allow-Origin', '*')
     return album_info
 
@@ -22,14 +22,16 @@ def scrape(url):
     # get album search results then find URL associated with first result
     results_page = requests.get(url)
     soup = BeautifulSoup(results_page.text, 'html.parser')
-    album = soup.find(class_='search_result_title').get('href')
+    album_url = soup.find(class_='search_result_title').get('href')
+    album_name = soup.find(class_='search_result_title').contents[0]
 
     # get album page and pull listed price
-    page = requests.get(disc_url_1 + album)
+    page = requests.get(disc_url_1 + album_url)
     soup = BeautifulSoup(page.text, 'html.parser')
     price = soup.find(class_='price').contents[0]
     album_art = soup.find(class_='thumbnail_center').find('img').get('src')
-    return  (price, album_art)
+    artist = soup.find(class_='profile').find('a').contents[0]
+    return  (price, album_art, album_name, artist)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
